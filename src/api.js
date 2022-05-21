1ad1ee766be05995549ed79ed00156a55fc9ff3e
Willian <willian.alves.b15@gmail.com>
2022-05-21 16:47:12 -0300

    feat: added user.login add api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const User = require('./database/controllers/user');
 // ...
 
 const app = express();
 
 app.use(express.json());
 
+app.post('/login', User.login);
 // ...
 
 // É importante exportar a constante `app`,
