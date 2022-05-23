68532cf3e3e73a2ef4a56f7295edf7bfb5e3e1a9
Willian <willian.alves.b15@gmail.com>
2022-05-23 14:31:54 -0300

    feat: services user create

--- src/database/services/user.js
+++ src/database/services/user.js
@@ -1,10 +1,19 @@
 const generateJWT = require('../utils/generateJWT');
+const { User } = require('../models');
 
 const login = async (obj) => {
   const token = generateJWT(obj);
   return token;
 };
 
+const create = async (body) => {
+  const { displayName, email, password, image } = body;
+  await User.create({ displayName, email, password, image });
+  const token = generateJWT({ displayName, email, image });
+  return token;
+};
+
 module.exports = {
   login,
+  create,
 };
