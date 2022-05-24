3fce355a4342f7733bfe5ab54469485c64960ce8
Willian <willian.alves.b15@gmail.com>
2022-05-23 21:47:24 -0300

    feat: added category controllers find

--- src/database/controllers/category.js
+++ src/database/controllers/category.js
@@ -5,6 +5,12 @@ const create = async (req, res) => {
   return res.status(201).json(category);
 };
 
+const find = async (_req, res) => {
+  const categoryAll = await Category.find();
+  return res.status(200).json(categoryAll);
+};
+
 module.exports = {
   create,
+  find,
 };
