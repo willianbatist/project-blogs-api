bb63e6dc05581333d0968e161f2b33c76854110c
Willian <willian.alves.b15@gmail.com>
2022-05-23 21:03:11 -0300

    feat: created checkName

---
+++ src/middleware/validationCategory.js
@@ -0,0 +1,11 @@
+const checkName = (req, res, next) => {
+  const { name } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: '"name" is required' });
+  }
+  return next();
+};
+
+module.exports = {
+  checkName,
+};
