fc84bc7245621b0620402f2a538ee46d6b9b56ba
Willian <willian.alves.b15@gmail.com>
2022-05-23 22:38:37 -0300

    feat: created models blogPost.js

---
+++ src/database/models/blogPost.js
@@ -0,0 +1,38 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define("BlogPost", {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    published: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    updated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    timestamps: false
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,{
+      foreignKey: 'userId', as: 'user'
+    })
+  };
+
+  return BlogPost;
+}
