ad18ea5f767d5b11c0430d22f3134eb1e5fab8d1
Willian <willian.alves.b15@gmail.com>
2022-05-23 22:40:01 -0300

    feat: update in user.js added associate hasMany with blogpost

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -25,5 +25,11 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost,{
+      foreignKey: 'id', as: 'blogPost'
+    })
+  };
+
   return User;
 }
