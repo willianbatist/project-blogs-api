ed3233d72ecb618df52bbfee36d25697927ba1a0
Willian <willian.alves.b15@gmail.com>
2022-05-25 17:17:09 -0300

    feat: created verifyPost and verifyEditPost

--- src/middleware/validationPost.js
+++ src/middleware/validationPost.js
@@ -1,4 +1,4 @@
-const { Category } = require('../database/models');
+const { Category, BlogPost, User } = require('../database/models');
 
 const checkPost = async (req, res, next) => {
     const { title, content, categoryIds } = req.body;
@@ -13,6 +13,31 @@ const checkPost = async (req, res, next) => {
     return next();
 };
 
+const verifyPost = async (req, res, next) => {
+  const { title, content } = req.body;
+  if (!title || !content) {
+    return res.status(400).json({ message: 'Some required fields are missing' });
+  }
+  return next();
+};
+
+const verifyEditPost = async (req, res, next) => {
+  const { id } = req.params;
+  const { payload } = req.user;
+  const findUser = await User.findOne({ where: { email: payload } });
+  const post = await BlogPost.findOne({ where: { id }, 
+    include: [
+    { model: User, as: 'user', attributes: { exclude: ['password'] } },
+    { model: Category, as: 'categories', through: { attributes: [] } },
+  ] });
+  if (findUser.id !== post.userId) {
+    return res.status(401).json({ message: 'Unauthorized user' });
+  }
+  return next();
+};
+
 module.exports = {
   checkPost,
+  verifyPost,
+  verifyEditPost,
 };
